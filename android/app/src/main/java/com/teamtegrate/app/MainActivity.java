27e76ff92429fee5b17042aece6e9f4f5fa2d191
D3monSausage <vivalopez06@gmail.com>
2025-07-19 09:05:19 -0700

    Initial commit with APK

---
+++ android/app/src/main/java/com/teamtegrate/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.teamtegrate.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,10 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.91cd77c434d94c9aa24033280dceab90',
-  appName: 'daily-team-sync',
+  appId: 'com.teamtegrate.app',
+  appName: 'TeamTegrate',
   webDir: 'dist',
-  server: {
-    url: 'https://91cd77c4-34d9-4c9a-a240-33280dceab90.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
